6b7f48c239200bbd0967364eebbb8055b3571df1
yunanhelmy <m.yunan.helmy@gmail.com>
2017-03-30 17:07:23 +0700

    add linebreak documentation rdoc

--- lib/sms_masking/phone_book.rb
+++ lib/sms_masking/phone_book.rb
@@ -2,6 +2,7 @@ module SmsMasking
   module PhoneBook
     
     # ==== Attributes
+    #
     # * +name+ : Contact Name
     # * +mobile+ : Recipient
     # * +group+ : Contact Group Name
@@ -10,6 +11,7 @@ module SmsMasking
     # * +desc+ (optional) : Contact Description
     #
     # ==== Example
+    #
     # sms.add_phone_book({
     #   name: "John Doe", 
     #   mobile: "085699999999", 
@@ -27,6 +29,7 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +id+ : Contact ID Number
     # * +name+ : Contact Name
     # * +mobile+ : Recipient
@@ -36,6 +39,7 @@ module SmsMasking
     # * +desc+ (optional) : Contact Description
     #
     # ==== Example
+    #
     # sms.edit_phone_book({
     #   id: 1,
     #   name: "John Doe", 
@@ -54,9 +58,11 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +id+ : Contact ID Number
     #
     # ==== Example
+    #
     # sms.delete_phone_book({id: 1})
     #
     def delete_phone_book(params = {})
@@ -67,10 +73,12 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +page+ : Page Number (default=1)
     # * +size+ : number of records per page (default=10)
     #
     # ==== Example
+    #
     # sms.list_phone_book({
     #   page: 1, 
     #   size: 10
@@ -84,11 +92,13 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +page+ : Page Number (default=1)
     # * +size+ : number of records per page (default=10)
     # * +group+ : Group Name
     #
     # ==== Example
+    #
     # sms.list_phone_book_by_group({
     #   page: 1, 
     #   size: 10,
@@ -103,9 +113,11 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +id+ : Contact ID Number
     #
     # ==== Example
+    #
     # sms.contact_detail({id: 1})
     #
     def contact_detail(params = {})
@@ -116,9 +128,11 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +name+ : Name to search
     #
     # ==== Example
+    #
     # sms.find_contact({name: "John Doe"})
     #
     def find_contact(params = {})
@@ -129,9 +143,11 @@ module SmsMasking
     end
 
     # ==== Attributes
+    #
     # * +number+ : Number to search
     #
     # ==== Example
+    #
     # sms.find_number({number: "085699999999"})
     #
     def find_number(params = {})
